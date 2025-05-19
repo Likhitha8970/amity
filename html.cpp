1b2608a7e86466e3d465a4d77ff1ddd41d0c2eac
likhi8970 <likhithagowda8970@gmail.com>
2025-05-19 09:38:15 +0530

    html program

---
+++ html.cpp
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My First HTML Page</title>
+</head>
+<body>
+    <h1>Welcome to My Web Page</h1>
+    <p>This is a paragraph of text on my very first HTML page.</p>
+    
+    <button onclick="alert('Hello, World!')">Click Me</button>
+</body>
+</html>
+
