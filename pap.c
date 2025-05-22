51b69f57e12c07ac738fb3477c1042024d9932f0
likhi8970 <likhithagowda8970@gmail.com>
2025-05-22 21:44:33 +0530

    likhi

---
+++ pap.c
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Person {
+private:
+    string name; // private data member
+
+public:
+    void setName(string n) {
+        name = n;
+    }
+    void display() {
+        cout << "Name: " << name << endl;
+    }
+};
+
+int main() {
+    Person p;
+    p.setName("Alice");
+    p.display();
+    return 0;
+}
+```
+
